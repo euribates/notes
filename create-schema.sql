27eef081f9f1fe635aaef8eca8d7f4791d28fa0e
Juan Ignacio Rodríguez de León <euribates@gmail.com>
2025-11-06 14:36:06 +0000

    Base de datos sqlite - Pruebas con FTS5

--- create-schema.sql
+++ create-schema.sql
@@ -5,3 +5,14 @@ CREATE TABLE note (
     topic text
     );
 
+
+CREATE VIRTUAL TABLE vnote USING fts5(
+    id_note,
+    title,
+    body,
+    topic,
+    content=note,
+    content_rowid=id_note
+);
+
+INSERT INTO vnote SELECT title, body, topic, id_note from note;
